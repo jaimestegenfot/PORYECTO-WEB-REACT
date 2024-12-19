51bd4df4744d1b3cc0f0524aceb6b3f014ef9373
jaimestegenfot <jaimemamanimamani2000@gmail.com>
2024-12-18 20:30:39 -0500

    mejorado al 100 xd

--- src/components/navegation.jsx
+++ src/components/navegation.jsx
@@ -1,7 +1,7 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './../css/Navigation.css';
 import './../css/principal.css';
+import './../scss/navegation.scss';
 import { Link } from 'react-router-dom';
 import icon from './../img/icon/icon.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,7 +9,7 @@ import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
   return (
-    <Navbar bg="light" expand="lg" fixed="top">
+    <Navbar expand="lg" fixed="top" className='Navbar'>
       <Container>
         <Navbar.Brand>
           <img
@@ -23,7 +23,7 @@ const Navigation = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto Navbar__links">
             <Link to="/" className="nav-link">FORMULARIO</Link>
             <Link to="/inicio" className="nav-link">INICO</Link>
             <Link to="/historia" className="nav-link">HISTORIA</Link>
